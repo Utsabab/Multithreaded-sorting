4f11bfc0bccd12dda560df43a0b398565e0f270a
Utsab06 <utsab.khakurel@bison.howard.edu>
2018-02-21 23:23:36 -0500

    working merge sort

--- mtsort.c
+++ mtsort.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int* nums;
+int nums[10] = {7, 78, 2, 6, 22, 13, 90, 52, 46, 11};
 
 void merger(int start,int mid,int end) {
-	length1 = mid - start;
-	length2 = end - (mid + 1);
+	int length1 = (mid - start) + 1;
+	int length2 = end - mid;
 	
-	left = (int *) malloc(length1 * sizeof(int));
-	right = (int *) malloc(length2 * sizeof(int));
+	int* left = (int *) malloc(length1 * sizeof(int));
+	int* right = (int *) malloc(length2 * sizeof(int));
 
 	int i;
 	for (i=0; i<length1; i++) {
-		left[i] = nums[i]
+		left[i] = nums[start + i];
 	}
 
 	for (i=0; i<length2; i++) {
-		right[i] = nums[i]
+		right[i] = nums[mid + i + 1];
 	}
 	int begin = start;
 	int head1=0, head2=0;
@@ -34,24 +34,35 @@ void merger(int start,int mid,int end) {
 	}
 	while (head1 < length1) {
 		nums[begin] = left[head1];
-		begin++;
 		head1++;
+		begin++;
 	}
 	while (head2 < length2) {
 		nums[begin] = right[head2];
-		begin++;
 		head2++;
+		begin++;
 	}
 }
 
-int* merge_sort(int start, int end) {
+void merge_sort(int start, int end) {
 	//int length = sizeof(arr) / sizeof(arr[0]);
 	//length = length / 2;
+	int mid;
 	if (start < end) {
-		int mid = (end - start) / 2;
+		mid = (end + start) / 2;
 		merge_sort(start, mid);
 		merge_sort(mid + 1, end);
 		merger(start, mid, end);
 	}
+}
+
+int main(int argc, char** argv)
+{
+	merge_sort(0, 9);
+	int i;
+	for (i=0;i<10;i++){
+		printf("%d \n", nums[i]);
+	}
+	return 0;
 }
 
